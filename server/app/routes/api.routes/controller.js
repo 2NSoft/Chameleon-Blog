eacd3be59ae237db8e965e89bffcbc5d6d6f7c5f
2NSoft <nikolay.i.nachev@gmail.com>
2017-09-30 11:32:33 +0300

    basic user support added

--- server/app/routes/api.routes/controller.js
+++ server/app/routes/api.routes/controller.js
@@ -1,16 +1,75 @@
 const init = ( data ) => {
     const controller = {
-        // getTowns( req, res ) {
-        //     return data.towns.getAll()
-        //         .then( ( towns ) => {
-        //             towns = towns.map( ( town ) => town.name );
-        //             return res.send( towns );
-        //         } )
-        //         .catch( ( err ) => {
-        //             return res.status( 500 )
-        //                 .send( 'Could not retrieve data! ' + err );
-        //         } );
-        // },
+        addUser( req, res ) {
+            const model = {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                stringProfilePicture: 'user.png',
+                nationality: req.body.nationality,
+                favourites: [],
+            };
+
+            if ( !data.users.validator.isValid( model ) ) {
+                return Promise.reject( res.status( 400 )
+                    .send( 'Data does not meet requirements!' ) );
+            }
+
+            return data.users.findUser( model.username )
+                .then( ( user ) => {
+                    if ( user ) {
+                        return Promise.reject( 'Username already exists!' );
+                    }
+                    return data.users.filter( { email: model.email } );
+                } )
+                .then( ( users ) => {
+                    if ( users.length ) {
+                        return Promise.reject( 'Email already used!' );
+                    }
+                    return data.users.create( model );
+                } )
+                .then( () => {
+                    return res.status( 200 ).send( 'User successfully added!' );
+                } )
+                .catch( ( err ) => {
+                    return res.status( 400 ).send( err );
+                } );
+        },
+        updateUser( req, res ) {
+            return data.users.findById( req.user.id )
+                .then( ( user ) => {
+                    user.username = req.user.username;
+                    user.email = req.body.email || user.email;
+                    user.password = req.body.password || user.password;
+                    user.nationality = req.body.nationality || user.nationality;
+                    if ( req.file ) {
+                        user.stringProfilePicture = req.file.filename;
+                    }
+                    if (!data.users.validator.isValid(user)) {
+                        return Promise
+                        .reject('Data does not meet requirements!');
+                    }
+                    return Promise.all( [user, data.users.filter( {
+                        email: user.email,
+                    } )] );
+                } )
+                .then( ( [validUser, users] ) => {
+                    const index = users
+                        .findIndex((user) =>
+                         user.id.toString() !== validUser.id.toString());
+                    if ( index !== -1 ) {
+                        return Promise.reject( 'E-mail already in use!' );
+                    }
+                    return data.users.updateById(validUser, req.body.password);
+                } )
+                .then( ( user ) => {
+                    return res.status(200)
+                    .send( 'User successfully updated!' );
+                } )
+                .catch( ( err ) => {
+                    return res.status( 400 ).send( err );
+                } );
+        },
     };
 
     return controller;
--- server/app/routes/api.routes/router.js
+++ server/app/routes/api.routes/router.js
@@ -2,9 +2,59 @@ const passport = require( 'passport' );
 
 const attach = (app, data) => {
     const controller = require( './controller' ).init( data );
-    // app.get( '/api/v1/towns', ( req, res ) => {
-    //     return controller.getTowns( req, res );
-    // } );
+
+    app.get( '/api/v1/auth', ( req, res ) => {
+        if ( req.user ) {
+            return res.status( 200 ).send( {
+                id: req.user.id,
+                username: req.user.username,
+                email: req.user.email,
+            } );
+        }
+        return res.status( 401 ).send( 'Not logged in!' );
+    });
+
+    app.post( '/api/v1/auth', ( req, res, next ) => {
+        passport.authenticate( 'local', ( error, user ) => {
+            if ( error ) {
+                return next( error );
+            }
+            if ( !user ) {
+                return res.status( 401 ).send( 'Log in failed!' );
+            }
+            req.logIn( user, ( err ) => {
+                if ( err ) {
+                    return next( err );
+                }
+                return res.status( 200 ).send( {
+                    id: req.user.id,
+                    username: req.user.username,
+                    email: req.user.email,
+                } );
+            } );
+            return true;
+        })( req, res, next );
+    } );
+
+    app.delete( '/api/v1/auth', ( req, res ) => {
+        req.logout();
+        if ( req.user ) {
+            return res.status(500)
+            .send( 'Something went wrong! Still Logged in!' );
+        }
+        return res.status( 200 ).send( 'Successfully logged out!' );
+    } );
+
+    app.post( '/api/v1/users', ( req, res ) => {
+        return controller.addUser( req, res );
+    } );
+
+    app.put( '/api/v1/users', ( req, res ) => {
+        if ( !req.user ) {
+            return res.status( 400 ).send( 'You need to be logged in!' );
+        }
+        return controller.updateUser( req, res );
+    } );
 };
 
 module.exports = attach;
